a37e116489cce3410ae47f7616b4735f77cef6f9
Musheer Alam <musheeralam360@gmail.com>
2023-07-20 07:13:10 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,63 @@
+// Create web server with node.js
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+var db = require('./lib/db.js');
+var shortid = require('shortid');
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query;
+    var pathname = url.parse(_url,true).pathname;
+    var title = queryData.id;
+
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        db.query(`SELECT * FROM topic`, function(error,topics){
+          title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = template.list(topics);
+          var html = template.HTML(title,list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`
+          );
+          response.writeHead(200);
+          response.end(html);
+        });
+      } else {
+        db.query(`SELECT * FROM topic`, function(error,topics){
+          if(error){
+            throw error;
+          }
+          db.query(`SELECT * FROM topic LEFT JOIN author ON topic.author_id=author.id WHERE topic.id=?`,[queryData.id], function(error2,topic){
+            if(error2){
+              throw error2;
+            }
+            title = topic[0].title;
+            var description = topic[0].description;
+            var list = template.list(topics);
+            var html = template.HTML(title,list,
+              `<h2>${sanitizeHtml(title)}</h2>
+              ${sanitizeHtml(description)}
+              <p>by ${sanitizeHtml(topic[0].name)}</p>
+              `,
+              ` <a href="/create">create</a>
+                <a href="/update?id=${queryData.id}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${queryData.id}">
+                  <input type="submit" value="delete">
+                </form>`
+            );
+            response.writeHead(200);
+            response.end(html);
+          });
+        });
+      }
+    } else if(pathname === '/create'){
+      db.query(`SELECT * FROM topic`, function(error,topics){
+        db.query('SELECT * FROM author',
